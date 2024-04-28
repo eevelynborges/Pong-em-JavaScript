756e037fb578014c779fc298fa43592fb3d8b704
Evelyn Borges <168367248+eevelynborges@users.noreply.github.com>
2024-04-28 16:28:39 -0300

    Update p5.collide2d.js

--- p5.collide2d.js
+++ p5.collide2d.js
@@ -1,12 +1,3 @@
-/*
-Repo: https://github.com/bmoren/p5.collide2D/
-Created by http://benmoren.com
-Some functions and code modified version from http://www.jeffreythompson.org/collision-detection
-Version v0.7.3 | June 22, 2020
-CC BY-NC-SA 4.0
-*/
-
-
 console.log("### p5.collide v0.7.3 ###")
 
 p5.prototype._collideDebug = false;
